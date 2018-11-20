16c7e2e9ba5694ee51fd3a8132ee0c29570fce7e
coolsidd <sidharth_0903@hotmail.com>
2018-11-21 01:02:00 +0530

    Ready for branching

--- adorer_app/lib/games.dart
+++ adorer_app/lib/games.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'game1.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -20,63 +21,5 @@ class MainScreenState extends State<MainScreen> {
           )
         ],
       );
-  }
-}
-
-class Game1 extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    return Game1State();
-  }
-}
-
-class Game1State extends State <Game1> {
-  int stage = 0;
-  Widget returnStage(){
-    switch(stage){
-      case 0:
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Tutorial"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Text("Tutorial Screen"),
-            FlatButton(
-              child: Text("PLAY"),
-              onPressed: (){
-                setState(() {stage++;});
-              },
-            )
-          ],
-        ),
-        );
-      break;
-      case 1:
-      return Board(1);
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Game1",
-      home: returnStage(),
-    );
-  }
-}
-
-class Board extends StatefulWidget{
-  final int level;
-  Board(this.level);
-  @override
-  State<StatefulWidget> createState(){
-    return BoardState();
-  }
-}
-
-class BoardState extends State<Board>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
